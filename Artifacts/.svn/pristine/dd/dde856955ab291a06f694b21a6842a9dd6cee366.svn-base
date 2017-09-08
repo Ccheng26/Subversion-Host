2fbad2975fb61e063275b7f3b21393d1092e1a6e
unknown <connie.cheng52@gmail.com>
2017-09-08 13:26:29 -0400

    removed incorrect directoy

--- Artifacts/.svn/pristine/dd/dde856955ab291a06f694b21a6842a9dd6cee366.svn-base
+++
@@ -1,9 +0,0 @@
-package taxes;
-
-public class EmployeeNameInvalidException extends Exception{
-	EmployeeNameInvalidException(){
-	}
-	public String toString() {
-		return "The employee name cannot be empty";
-	}
-}
--- Artifacts/branches/CalculatorSimulator/ExceptionHandlingJavaActivity/src/taxes/CountryNotValidException.java
+++
@@ -1,9 +0,0 @@
-package taxes;
-
-public class CountryNotValidException extends Exception {
-	CountryNotValidException(){
-	}
-	public String toString() {
-		return "The employee should be an Indian citizen for calculating tax";
-	}
-}
